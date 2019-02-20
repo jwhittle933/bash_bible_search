064d87080b8b38daa0c8fa2350d512e5365dd4c5
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-02-19 20:39:57 -0500

    add dynamic chapter cURL

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ api_key=3f3c0c2f26e91bac4a9294bc0069a44d
 bible_books='{"genesis": "GEN", "exodus": "EXO", "leviticus": "LEV"}'
 echo "What book would you like to search?"
 read read_book
+echo "What chapter would you like to search?"
+read read_chapter
 
 case $read_book in
 	genesis)
@@ -19,7 +21,7 @@ esac
 
 
 books=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/books --header 'api-key: 3f3c0c2f26e91bac4a9294bc0069a44d' | jq '.data' | jq '.[].id')
-passage=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/passages/${book_choice}.1 --header 'api-key: 3f3c0c2f26e91bac4a9294bc0069a44d' | jq '.data.content')
+passage=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/passages/${book_choice}.${read_chapter} --header 'api-key: 3f3c0c2f26e91bac4a9294bc0069a44d' | jq '.data.content')
 
 for i in $books
 do
