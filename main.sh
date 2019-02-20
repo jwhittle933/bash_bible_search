790ff2d40dfe3bc23560ecc7b418ae47ecbae898
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-02-19 21:25:43 -0500

    add readme

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 # !/bin/sh
 . ./functions.sh
+. ./api_key.sh
 
 convert_user_input_book "This is a test"
 
-api_key=3f3c0c2f26e91bac4a9294bc0069a44d
 bible_books='{"genesis": "GEN", "exodus": "EXO", "leviticus": "LEV"}'
 echo "What book would you like to search?"
 read read_book
@@ -23,7 +23,7 @@ case $read_book in
 esac
 
 
-books=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/books --header 'api-key: 3f3c0c2f26e91bac4a9294bc0069a44d' | jq '.data' | jq '.[].id')
+books=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/books --header "api-key: $api_key" | jq '.data' | jq '.[].id')
 passage=$(curl --request GET --url https://api.scripture.api.bible/v1/bibles/0bc8836afa7427fa-01/passages/${book_choice}.${read_chapter} --header 'api-key: 3f3c0c2f26e91bac4a9294bc0069a44d' | jq '.data.content')
 
 for i in $books
